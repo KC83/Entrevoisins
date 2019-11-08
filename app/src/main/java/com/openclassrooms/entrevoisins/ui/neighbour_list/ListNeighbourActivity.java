5bfb283427463a57279476f498a1040da9a103e6
Kelly <kellyc83780@gmail.com>
2019-11-08 16:00:45 +0100

    Développement des fonctionnalités

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivity.java
@@ -0,0 +1,66 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.openclassrooms.entrevoisins.R;
+
+import java.util.regex.Matcher;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ListNeighbourActivity extends AppCompatActivity {
+
+    // UI Components
+    @BindView(R.id.tabs)
+    TabLayout mTabLayout;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.container)
+    ViewPager mViewPager;
+
+    ListNeighbourPagerAdapter mPagerAdapter;
+    SharedPreferences mPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_neighbour);
+        ButterKnife.bind(this);
+
+        mPreferences = getSharedPreferences("ENTREVOISINS",MODE_PRIVATE);
+        //mPreferences.edit().clear().commit();
+        mPreferences.edit().putInt("TAB",0).apply();
+
+        setSupportActionBar(mToolbar);
+        mPagerAdapter = new ListNeighbourPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+        mTabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mPreferences.edit().putInt("TAB",tab.getPosition()).apply();
+                mViewPager.setAdapter(mPagerAdapter);
+
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
+    }
+}
