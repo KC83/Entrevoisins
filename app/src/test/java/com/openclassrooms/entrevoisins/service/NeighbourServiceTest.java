56db81e78bd921a7ae11c679f293af2fd659bfcd
KC83 <56731166+kc83@users.noreply.github.com>
2019-11-18 15:25:20 +0100

    Tests unitaires

--- app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
+++ app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
@@ -9,10 +9,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test on Neighbour service
@@ -39,5 +42,56 @@ public class NeighbourServiceTest {
         Neighbour neighbourToDelete = service.getNeighbours().get(0);
         service.deleteNeighbour(neighbourToDelete);
         assertFalse(service.getNeighbours().contains(neighbourToDelete));
+    }
+
+    @Test
+    public void getNeighbourDetailWithSuccess() {
+        Neighbour neighbour = service.getNeighbours().get(0);
+        assertNotNull(neighbour.getId());
+        assertNotNull(neighbour.getAvatarUrl());
+        assertNotNull(neighbour.getName());
+    }
+
+    @Test
+    public void addNeighbourInFavorites() {
+        List<Neighbour> favoritesNeighbours = new ArrayList<Neighbour>();
+
+        Neighbour neighbour = service.getNeighbours().get(0);
+        neighbour.setFavorite(true);
+
+        favoritesNeighbours.add(neighbour);
+        assertTrue(favoritesNeighbours.contains(neighbour));
+        assertTrue(neighbour.isFavorite());
+    }
+
+    @Test
+    public void removeNeighbourFromFavorites() {
+        List<Neighbour> favoritesNeighbours = new ArrayList<Neighbour>();
+
+        Neighbour neighbour = service.getNeighbours().get(0);
+        neighbour.setFavorite(true);
+
+        // Add in favorite
+        favoritesNeighbours.add(neighbour);
+        assertTrue(favoritesNeighbours.contains(neighbour));
+        assertTrue(neighbour.isFavorite());
+
+        // Remove from favorite
+        neighbour.setFavorite(false);
+        favoritesNeighbours.remove(neighbour);
+        assertFalse(favoritesNeighbours.contains(neighbour));
+        assertFalse(neighbour.isFavorite());
+    }
+
+    @Test
+    public void getFavoritesNeighboursWithSuccess() {
+        List<Neighbour> favoritesNeighbours = new ArrayList<Neighbour>();
+
+        Neighbour neighbour = service.getNeighbours().get(0);
+        neighbour.setFavorite(true);
+        favoritesNeighbours.add(neighbour);
+
+        assertTrue(favoritesNeighbours.contains(neighbour));
+        assertTrue(neighbour.isFavorite());
     }
 }
